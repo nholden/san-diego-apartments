bbb66b1d44a952fe4764dbdcf426d77f2f605b13
Nick Holden <nick@realhq.com>
2016-06-04 21:28:20 -0400

    Continue fixing singular/plural in email subject line

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -10,11 +10,11 @@ class AlertMailer < ApplicationMailer
 
     email_with_name = %("#{@recipient.first_name} #{@recipient.last_name}" <#{@recipient.email}>)
 
-    subject = "Your recent San Diego updates ("
-    subject += "#{@new_units.length} new #{pluralize(@new_units.length, "unit")}, " if @new_units.present?
-    subject += "#{@rent_changed_units.length} updated #{pluralize(@rent_changed_units.length, "rent")}, " if @rent_changed_units.present?
-    subject += "#{@available_changed_units.length} updated available #{pluralize(@available_changed_units.length, "date")}, " if @available_changed_units.present?
-    subject.gsub!(/, $/, ")")
+    updated_elements = []
+    updated_elements.push(pluralize(@new_units.length, "new unit")) if @new_units.present?
+    updated_elements.push(pluralize(@rent_changed_units.length, "updated rent")) if @rent_changed_units.present?
+    updated_elements.push(pluralize(@available_changed_units.length, "date")) if @available_changed_units.present?
+    subject = "Your recent San Diego updates (" + updated_elements.join(", ") + ")"
 
     mail(to: email_with_name, subject: subject)
   end

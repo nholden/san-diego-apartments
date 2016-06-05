b661ba82a051ffd6a7af9fd59a50640d1e02dbaf
Nick Holden <nick@realhq.com>
2016-06-04 20:13:52 -0400

    Update hourly email to, from, and subject lines

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,5 +1,5 @@
 class AlertMailer < ApplicationMailer
-  default from: "apartmentbot@nickholden.io"
+  default from: %("Apartment Bot" <apartmentbot@nickholden.io>)
 
   def hourly_email(recipient)
     @recipient = recipient
@@ -7,6 +7,14 @@ class AlertMailer < ApplicationMailer
     @rent_changed_units = RentAlert.units_from_last_hour
     @available_changed_units = AvailableAlert.units_from_last_hour
 
-    mail(to: @recipient.email, subject: "Your recent San Diego apartment updates")
+    email_with_name = %("#{@recipient.first_name} #{@recipient.last_name}" <#{@recipient.email}>)
+
+    subject = "Your recent San Diego updates ("
+    subject += "#{@new_units.length} new units, " if @new_units.present?
+    subject += "#{@rent_changed_units.length} updated rents, " if @rent_changed_units.present?
+    subject += "#{@available_changed_units.length} updated available dates" if @available_changed_units.present?
+    subject.gsub(/, $/, ")")
+
+    mail(to: email_with_name, subject: subject)
   end
 end
